b63c3857abe7191a06d4a6f5e522261def713aa4
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-08-10 14:19:48 +0530

    clean code

--- deeplearningfornlp-main/Chapter1/listing_1_10-layer_MLP_sentiment_classifier.py
+++ deeplearningfornlp-main/Chapter1/listing_1_10-layer_MLP_sentiment_classifier.py
@@ -7,20 +7,20 @@ import pandas as pd
 import sys
 
 data = pd.read_csv(sys.argv[1],sep='\t') 
-docs=data["text"] <3>
+docs=data["text"] 
 
-tokenizer = Tokenizer() <4>
-tokenizer.fit_on_texts(docs) <5>
+tokenizer = Tokenizer() 
+tokenizer.fit_on_texts(docs) 
 
-X_train = tokenizer.texts_to_matrix(docs, mode='binary') <6>
+X_train = tokenizer.texts_to_matrix(docs, mode='binary')
 y_train=np_utils.to_categorical(data["label"])
 
-input_dim = X_train.shape[1] <7>
+input_dim = X_train.shape[1] 
 nb_classes = y_train.shape[1]
 
 model = Sequential()
 model.add(Dense(128, input_dim=input_dim))
-model.add(Activation('sigmoid')) <8>
+model.add(Activation('sigmoid')) 
 model.add(Dense(128))
 model.add(Activation('sigmoid'))
 model.add(Dense(128))
@@ -39,13 +39,13 @@ model.add(Dense(128))
 model.add(Activation('sigmoid'))
 model.add(Dense(128))
 model.add(Activation('sigmoid'))
-model.add(Dense(nb_classes)) <9>
-model.add(Activation('softmax')) <10>
+model.add(Dense(nb_classes)) 
+model.add(Activation('softmax')) 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
-              metrics=['accuracy']) <11>
+              metrics=['accuracy'])
 
 print("Training...")
 model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.1,
-[CA]shuffle=False,verbose=2) <12>
+[CA]shuffle=False,verbose=2)
 
