887cff7433e68a4259b188f3078feabcf5378356
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2023-08-11 05:18:14 +0530

    code cleaned and run

--- deeplearningfornlp-main/Chapter1/listing_1_A_simple_perceptron-based_document_classifier.py
+++ deeplearningfornlp-main/Chapter1/listing_1_A_simple_perceptron-based_document_classifier.py
@@ -1,31 +1,40 @@
-from sklearn.linear_model import Perceptron <1>
-from sklearn.datasets import fetch_20newsgroups <2>
+# %%
 
-categories = ['alt.atheism', 'sci.med'] <3>
+from sklearn.linear_model import Perceptron
+from sklearn.datasets import fetch_20newsgroups
 
-train = fetch_20newsgroups(
-[CA]subset='train',categories=categories, shuffle=True) <4>
+categories = ["alt.atheism", "sci.med"]
 
-perceptron = Perceptron(max_iter=100) <5>
+train = fetch_20newsgroups(subset="train", categories=categories, shuffle=True)
 
-from sklearn.feature_extraction.text import CountVectorizer <6>
+perceptron = Perceptron(max_iter=100)
+
+from sklearn.feature_extraction.text import CountVectorizer
+
 cv = CountVectorizer()
 X_train_counts = cv.fit_transform(train.data)
 
-from sklearn.feature_extraction.text import TfidfTransformer <7>
+from sklearn.feature_extraction.text import TfidfTransformer
+
 tfidf_tf = TfidfTransformer()
 X_train_tfidf = tfidf_tf.fit_transform(X_train_counts)
 
-perceptron.fit(X_train_tfidf,train.target) <8>
+perceptron.fit(X_train_tfidf, train.target)
 
-test_docs = ['Religion is widespread, even in modern times', 'His kidney
-[CA]failed','The pope is a controversial leader', 'White blood cells fight
-[CA]off infections','The reverend had a heart attack in church'] <9>
+test_docs = [
+    "Religion is widespread, even in modern times",
+    "His kidney failed",
+    "The pope is a controversial leader",
+    "White blood cells fight off infections",
+    "The reverend had a heart attack in church",
+]
 
-X_test_counts = cv.transform(test_docs) <10>
+X_test_counts = cv.transform(test_docs)
 X_test_tfidf = tfidf_tf.transform(X_test_counts)
 
-pred = perceptron.predict(X_test_tfidf) <11>
+pred = perceptron.predict(X_test_tfidf)
 
-for doc, category in zip(test_docs, pred): <12>
-    print('%r => %s' % (doc, train.target_names[category]))
+for doc, category in zip(test_docs, pred):
+    print("%r => %s" % (doc, train.target_names[category]))
+
+# %%
